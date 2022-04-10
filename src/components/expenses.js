94e43d57ab7772439834f4adc77be2a4a67171a0
Ramos Fonseca <beidseitig@gmail.com>
2022-04-10 16:54:30 -0300

    Requisito 7 - criando a tabela de despesas

--- src/components/expenses.js
+++ src/components/expenses.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
+import { fetchAPIThunk } from '../actions';
 
 class Expenses extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      coin: 'USD',
+      method: '',
+      category: '',
+      description: '',
+    };
+  }
+
+  componentDidMount = async () => {
+    this.getCurrency();
+  }
+
+  getCurrency = async () => {
+    const { fetchCurrencies } = this.props;
+    await fetchCurrencies();
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { value, coin, method, category, description } = this.state;
     const { currencies } = this.props;
     return (
       <section>
         <label htmlFor="value">
           Valor:
-          <input type="number" name="value" id="value" data-testid="value-input" />
+          <input
+            type="number"
+            name="value"
+            value={ value }
+            id="value"
+            onChange={ this.handleChange }
+            data-testid="value-input"
+          />
         </label>
 
-        <label htmlFor="currency">
+        <label htmlFor="coin">
           Moeda:
-          <select name="currency" id="currency" data-testid="currency-input">
+          <select
+            name="coin"
+            value={ coin }
+            id="coin"
+            onChange={ this.handleChange }
+            data-testid="currency-input"
+            label="Moeda"
+          >
             {
               currencies.map((item, index) => (
                 <option key={ index } value={ item }>{item}</option>
@@ -25,21 +66,35 @@ class Expenses extends React.Component {
 
         <label htmlFor="method">
           Método de pagamento:
-          <select name="method" id="method" data-testid="method-input">
-            <option>Dinheiro</option>
-            <option>Cartão de crédito</option>
-            <option>Cartão de débito</option>
+          <select
+            name="method"
+            value={ method }
+            id="method"
+            onChange={ this.handleChange }
+            data-testid="method-input"
+          >
+            <option>Escolha a forma de pagamento</option>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
           </select>
         </label>
 
-        <label htmlFor="tag">
+        <label htmlFor="category">
           Categoria:
-          <select name="tag" id="tag" data-testid="tag-input">
-            <option>Alimentação</option>
-            <option>Lazer</option>
-            <option>Trabalho</option>
-            <option>Transporte</option>
-            <option>Saúde</option>
+          <select
+            name="category"
+            value={ category }
+            id="category"
+            onChange={ this.handleChange }
+            data-testid="tag-input"
+          >
+            <option>Escolha o tipo de despesa</option>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
           </select>
         </label>
 
@@ -48,10 +103,13 @@ class Expenses extends React.Component {
           <input
             type="text"
             name="description"
+            value={ description }
             id="description"
+            onChange={ this.handleChange }
             data-testid="description-input"
           />
         </label>
+        <button type="button">Adicionar despesa</button>
       </section>
     );
   }
@@ -61,8 +119,13 @@ const mapStateToProps = (state) => ({
   currencies: state.wallet.currencies,
 });
 
-Expenses.propTypes = {
-  currencies: propTypes.string.isRequired,
-};
+const mapDispatchToProps = (dispatch) => ({
+  fetchCurrencies: () => dispatch(fetchAPIThunk()),
+});
 
-export default connect(mapStateToProps)(Expenses);
+Expenses.propTypes = {
+  currencies: propTypes.string,
+  fetchCurrencies: propTypes.func,
+}.isRequired;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Expenses);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import propTypes from 'prop-types';
 import Header from '../components/header';
-import { fetchAPIThunk } from '../actions';
 import Expenses from '../components/expenses';
+import Saved from '../components/saved';
 
 class Wallet extends React.Component {
-  componentDidMount = async () => {
-    this.getCurrency();
-  }
-
-  getCurrency = async () => {
-    const { currencies } = this.props;
-    await currencies();
-  };
-
   render() {
     return (
       <>
         <Header />
-        ;
         <Expenses />
+        <Saved />
       </>);
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  currencies: () => dispatch(fetchAPIThunk()),
-});
-
-Wallet.propTypes = {
-  currencies: propTypes.func.isRequired,
-};
-
-export default connect(null, mapDispatchToProps)(Wallet);
+export default Wallet;
